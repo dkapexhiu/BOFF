f4c44a68a2b3c15c214d94866a53c081fe17d3c6
Daniel <daniel.kapexhiu@gmail.com>
2018-07-17 10:15:31 +0200

    webapp install prompt

--- js/script.js
+++ js/script.js
@@ -135,20 +135,34 @@ note2.addEventListener('click', function() {
   notification2();
 });
 
-window.addEventListener('beforeinstallprompt', function(e) {
-  // beforeinstallprompt Event fired
+let deferredPrompt;
 
-  // e.userChoice will return a Promise.
-  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
-  e.userChoice.then(function(choiceResult) {
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+});
 
-    console.log(choiceResult.outcome);
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
 
-    if(choiceResult.outcome == 'dismissed') {
-      console.log('User cancelled home screen install');
-    }
-    else {
-      console.log('User added to home screen');
-    }
-  });
+window.addEventListener('appinstalled', (evt) => {
+  app.logEvent('a2hs', 'installed');
 });
